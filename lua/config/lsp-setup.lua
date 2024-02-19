03fb58308a196dd1220802effab60d94f994aaee
JasonGrace2282 <aarush.deshpande@gmail.com>
2024-02-19 10:02:19 -0500

    Patched errors

--- lua/config/lsp-setup.lua
+++ lua/config/lsp-setup.lua
@@ -1,21 +1,16 @@
-vim.keymap.set('n', 'gl', '<cmd>lua vim.diagnostic.open_float()<cr>')
-vim.keymap.set('n', '[d', '<cmd>lua vim.diagnostic.goto_prev()<cr>')
-vim.keymap.set('n', ']d', '<cmd>lua vim.diagnostic.goto_next()<cr>')
-
 vim.api.nvim_create_autocmd('LspAttach', {
   desc = 'LSP actions',
   callback = function(event)
-    local opts = {buffer = event.buf}
-    vim.keymap.set('n', 'K', '<cmd>lua vim.lsp.buf.hover()<cr>', opts)
-    vim.keymap.set('n', 'gd', '<cmd>lua vim.lsp.buf.definition()<cr>', opts)
-    vim.keymap.set('n', 'gD', '<cmd>lua vim.lsp.buf.declaration()<cr>', opts)
-    vim.keymap.set('n', 'gi', '<cmd>lua vim.lsp.buf.implementation()<cr>', opts)
-    vim.keymap.set('n', 'go', '<cmd>lua vim.lsp.buf.type_definition()<cr>', opts)
-    vim.keymap.set('n', 'gr', '<cmd>lua vim.lsp.buf.references()<cr>', opts)
-    vim.keymap.set('n', 'gs', '<cmd>lua vim.lsp.buf.signature_help()<cr>', opts)
-    vim.keymap.set('n', '<F2>', '<cmd>lua vim.lsp.buf.rename()<cr>', opts)
-    vim.keymap.set({'n', 'x'}, '<F3>', '<cmd>lua vim.lsp.buf.format({async = true})<cr>', opts)
-    vim.keymap.set('n', '<F4>', '<cmd>lua vim.lsp.buf.code_action()<cr>', opts)
+    local nmap = function (bind, cmd, desc) vim.keymap.set('n', bind, cmd, { buffer = event.buf, desc = desc }) end
+
+    nmap('K', vim.lsp.buf.hover, "VS Code-like hovering")
+    nmap('gd', ':Telescope lsp_definitions', "Definition of word")
+    nmap('gi', ':Telescope lsp_implementation', "See implementation")
+    nmap('go', ':Telescope lsp_type_definition', "See definition of type (C-like languages)")
+    nmap('gr', ':Telescope lsp_references', "See references")
+    nmap('gK', ':Telescope lsp_signature_help', "See signature")
+    nmap('<leader>rn', ':Telescope lsp_rename', "Rename something")
+    nmap('<leader>cf', ':Telescope lsp_format({async = true})', "Format the file")
   end
 })
 
@@ -33,7 +28,7 @@ require('mason-lspconfig').setup({
     "rust_analyzer",
     "lua_ls",
     "ruff_lsp",
-    "python-lsp-server"
+    "pylsp"
   },
   handlers = {
     default_setup,
