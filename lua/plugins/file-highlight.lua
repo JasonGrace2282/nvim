a22456b9e2c194612b49ab7bf39eb003cf3b1c0f
JasonGrace2282 <aarush.deshpande@gmail.com>
2024-02-26 22:31:28 -0500

    Add kitty syntax highlight

--- lua/plugins/file-highlight.lua
+++ lua/plugins/file-highlight.lua
@@ -1,5 +1,11 @@
 return {
-  "theRealCarneiro/hyprland-vim-syntax",
-  dependencies = { "nvim-treesitter/nvim-treesitter" },
-  ft = "hypr",
+  {
+    "theRealCarneiro/hyprland-vim-syntax",
+    dependencies = { "nvim-treesitter/nvim-treesitter" },
+    ft = "hypr",
+  },
+  {
+    'fladson/vim-kitty',
+    ft = 'kitty'
+  }
 }
