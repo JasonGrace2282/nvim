1ba71c0a94c1b939df53e934af9c7eb517b43770
JasonGrace2282 <aarush.deshpande@gmail.com>
2024-02-29 07:50:59 -0500

    Move projects to lua/config/projects.lua

--- lua/plugins/code_runner.lua
+++ lua/plugins/code_runner.lua
@@ -14,17 +14,6 @@ return {
         "$dir/$fileNameWithoutExt"
       }
     },
-    project = {
-      ["~/repos/learn/todo"] = {
-        name = "todoApp",
-        description = "A todo app in Rust",
-        command = "cargo run"
-      },
-      ["~/repos/ion"] = {
-        name = "Ion",
-        description = "Intranet for TJHSST",
-        command = "~/repos/ion/scripts/run-open.sh"
-      }
-    }
+    project = require('config.projects')
   }
 }
