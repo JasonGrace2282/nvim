aff72d131edc13e750e56df7cebb778d533e5012
JasonGrace2282 <aarush.deshpande@gmail.com>
2024-03-04 22:45:01 -0500

    Improve noice and indents

--- lua/plugins/noice.lua
+++ lua/plugins/noice.lua
@@ -1,7 +1,28 @@
 return {
   "folke/noice.nvim",
   event = "VeryLazy",
-  opts = {},
+  opts = {
+    popupmenu = {
+      enabled = true, -- enables the Noice popupmenu UI
+      backend = "cmp", -- backend to use to show regular cmdline completions
+      relative = "editor",
+      position = {
+        row = 8,
+        col = "50%",
+      },
+      size = {
+        width = 60,
+        height = 10,
+      },
+      border = {
+        style = "rounded",
+        padding = { 0, 1 },
+      },
+      win_options = {
+        winhighlight = { Normal = "Normal", FloatBorder = "DiagnosticInfo" },
+      },
+    },
+  },
   dependencies = {
     -- if you lazy-load any plugin below, make sure to add proper `module="..."` entries
     "MunifTanjim/nui.nvim",
