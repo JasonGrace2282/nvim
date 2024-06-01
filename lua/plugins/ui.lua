a992e3b953e20c4094129e547f4db0ccc0f46ee1
JasonGrace2282 <aarush.deshpande@gmail.com>
2024-06-01 16:09:27 -0400

    Let noice do some lsp stuff

--- lua/plugins/ui.lua
+++ lua/plugins/ui.lua
@@ -28,6 +28,13 @@ return {
           level = vim.log.levels.INFO,
           replace = true
         }
+      },
+      lsp = {
+        override = {
+          ['vim.lsp.util.convert_input_to_markdown_lines'] = true,
+          ['vim.lsp.util.stylize_markdown'] = true,
+          ['cmp.entry.get_documentation'] = true,
+        },
       }
     },
     dependencies = {
