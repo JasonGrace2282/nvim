a97e1609fc0532b2e3c5bcda96b1addeca494ce9
JasonGrace2282 <aarush.deshpande@gmail.com>
2024-08-09 08:28:52 -0400

    Ignore min.js files telescope

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -2,5 +2,12 @@ return {
   'nvim-telescope/telescope.nvim',
   tag = '0.1.5',
   dependencies = { 'nvim-lua/plenary.nvim' },
-  priority=100
+  priority = 100,
+  opts = {
+    defaults = {
+      file_ignore_patterns = {
+        ".*.min.*",
+      }
+    }
+  }
 }
