d887a1660af76b80c134f92cd991fede4d0d51ac
JasonGrace2282 <aarush.deshpande@gmail.com>
2026-09-05 11:37:46 -0400

    remove some ensure_installed

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -2,30 +2,19 @@ return {
   "nvim-treesitter/nvim-treesitter",
   build = ":TSUpdate",
   opts = {
-    ensure_installed = { "c", "lua", "vim", "vimdoc", "query", "elixir", "heex", "javascript", "html", "wgsl" },
+    ensure_installed = {
+      "c",
+      "lua",
+      "vim",
+      "vimdoc",
+      "query",
+      "javascript",
+      "html",
+      "markdown",
+      "markdown_inline",
+    },
     sync_install = false,
     highlight = { enable = true },
     indent = { enable = true },
   },
-  config = function(_, opts)
-    require("nvim-treesitter.configs").setup(opts)
-    local parsers = require("nvim-treesitter.parsers")
-
-    -- wgsl highlighting
-    local parser_config = parsers.get_parser_configs()
-    parser_config["wgsl"] = {
-      install_info = {
-        url = "https://github.com/szebniok/tree-sitter-wgsl",
-        files = { "src/parser.c" },
-      },
-    }
-
-    -- mdx highlighting
-    vim.filetype.add({
-      extension = {
-        mdx = "mdx",
-      },
-    })
-    vim.treesitter.language.register("markdown", "mdx")
-  end,
 }
